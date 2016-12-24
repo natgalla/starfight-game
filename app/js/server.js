fd4a24f097f1da121e4120f56d805aa6284a99e6
Nathan <natgalla@gmail.com>
2016-12-23 23:41:12 -0500

    Prep for path overhaul

--- app/js/server.js
+++ app/js/server.js
@@ -1501,8 +1501,16 @@ app.get("/login", function(req, res) {
   res.render('login');
 });
 
+app.post("/login", function(req, res) {
+  res.redirect("/menu");
+});
+
 app.get("/menu", function(req, res) {
   res.render('menu');
+});
+
+app.post("/menu", function(req, res) {
+  res.redirect("/game");
 });
 
 app.get("/game", function(req, res) {
