b1691e30752c589bdc8be6770b8ddd5e17151d6b
Nathan <natgalla@gmail.com>
2017-01-22 12:49:37 -0600

    restructuring of app folder

--- js/client.js
+++
@@ -1,9 +0,0 @@
-function onMessage(text) {
-  var list = document.getElementById("status");
-  var el = document.createElement('li');
-  el.innerHTML = text;
-  list.appendChild(el);
-}
-
-onMessage('Hi');
-onMessage('I am connected');
--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-$(".title").hide();
-$("#title").fadeIn(1200, function() {
-  $("#subtitle").fadeIn(1000);
-});
