e88c865570f9ad508e7d38558e67fb4fe928919f
Nathan <natgalla@gmail.com>
2017-01-28 12:29:06 -0600

    Fixed database link

--- app.js
+++ app.js
@@ -1333,7 +1333,7 @@ let port = process.env.PORT || 8080;
 // mongodb connection
 let mongoUri = 'mongodb://heroku_rmsqzvkd:oavs0o32a02l6vc163tbennr9s@ds119608.mlab.com:19608/heroku_rmsqzvkd';
 let localUri = 'mongodb://localhost:27017/starfire';
-mongoose.connect(localUri);
+mongoose.connect(mongoUri);
 let db = mongoose.connection;
 
 // mongo error
